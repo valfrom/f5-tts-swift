b7bd532c8ac2da2672cc87311888807666c86b79
Valerii Ivanov <valfrom@gmail.com>
2026-08-19 15:03:48 +0200

    Use eval-pause MLX dependencies

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.18.0"),
-        .package(url: "https://github.com/huggingface/swift-transformers", from: "0.1.13"),
+        .package(url: "https://github.com/valfrom/mlx-swift.git", branch: "eval-pause"),
+        .package(url: "https://github.com/huggingface/swift-transformers", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-        .package(url: "https://github.com/valfrom/vocos-swift.git", from: "0.0.2")
+        .package(url: "https://github.com/valfrom/vocos-swift.git", branch: "main")
     ],
     targets: [
         .target(
